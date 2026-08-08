36b19075844ac2825a64d9f361141d2dd0aa242f
abood.jarrar <abd.a.jarrar@gmail.com>
2026-08-08 21:23:21 +0300

    finalyze implementing employee and department class

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -9,9 +9,10 @@ namespace EmployeeManagementSystem.Models
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public int DepartmentId {  get; set; }
         public DateOnly HireDate { get; set; }
         public decimal Salary { get; set; }
+        public List<string> Skills { get; set; } = new List<string>();
     }
 }
